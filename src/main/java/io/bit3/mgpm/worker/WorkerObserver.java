24de3704bd6bce17efcd09742e97d583a6969fee
jcl4332 <72151787+jcl4332@users.noreply.github.com>
2024-06-17 22:36:49 +0200

    Delete src/main/java/io/bit3/mgpm directory

--- src/main/java/io/bit3/mgpm/worker/WorkerObserver.java
+++
@@ -1,9 +0,0 @@
-package io.bit3.mgpm.worker;
-
-public interface WorkerObserver {
-  void start(Worker worker);
-
-  void activity(Activity activity, Worker worker);
-
-  void end(Worker worker);
-}
