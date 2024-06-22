4d9411305db17053ea3fa5d4560603dffbd36050
jcl4332 <72151787+jcl4332@users.noreply.github.com>
2024-06-22 16:13:27 +0200

    Add files via upload

--- src/main/java/io/bit3/mgpm/gui/GuiApplication.java
+++ src/main/java/io/bit3/mgpm/gui/GuiApplication.java
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 package io.bit3.mgpm.gui;
-
-import javax.swing.JOptionPane;
 
 import io.bit3.mgpm.cmd.Args;
 import io.bit3.mgpm.config.Config;
+
+import javax.swing.*;
 
 public class GuiApplication {
   private final Args args;
@@ -19,25 +18,3 @@ public class GuiApplication {
     JOptionPane.showMessageDialog(null, "GUI is not implemented yet :-(");
   }
 }
-=======
-package io.bit3.mgpm.gui;
-
-import javax.swing.JOptionPane;
-
-import io.bit3.mgpm.cmd.Args;
-import io.bit3.mgpm.config.Config;
-
-public class GuiApplication {
-  private final Args args;
-  private final Config config;
-
-  public GuiApplication(Args args, Config config) {
-    this.args = args;
-    this.config = config;
-  }
-
-  public void run() {
-    JOptionPane.showMessageDialog(null, "GUI is not implemented yet :-(");
-  }
-}
->>>>>>> 12f34cdc3590fe6d4898108340a93beb51687140
